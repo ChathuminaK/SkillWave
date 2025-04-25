2f3f02eb1b3f2e60ad817bd229ef53b2cc376c39
Chathumina Kaushal <161899336+ChathuminaK@users.noreply.github.com>
2025-04-26 01:18:47 +0530

    Initial commit

---
+++ backend/src/main/java/com/example/SkillWave/SkillWaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.SkillWave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillWaveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/SkillWave/SkillWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SkillWave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
