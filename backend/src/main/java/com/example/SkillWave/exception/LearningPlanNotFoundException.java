6bacb24ed1d093cd9393aebbf6be5ee4946e91f8
Chathumina Kaushal <161899336+ChathuminaK@users.noreply.github.com>
2025-05-17 01:18:01 +0530

    Delete backend directory

--- backend/src/main/java/com/example/SkillWave/exception/LearningPlanNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.SkillWave.exception;
-
-public class LearningPlanNotFoundException extends RuntimeException {
-    public LearningPlanNotFoundException(Long id) {
-        super("Learning plan not found with id: " + id);
-    }
-}
--- backend/src/main/java/com/example/SkillWave/security/SecurityConfig.java
+++
@@ -1,5 +0,0 @@
-package com.example.SkillWave.security;
-
-public class SecurityConfig {
-    
-}
